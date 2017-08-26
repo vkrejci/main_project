edc4c05c8454db463468e126470b8130d1209a28
Vašek <vaclavkrejcii@gmail.com>
2017-08-26 19:19:09 +0200

    'Ahoj'

---
+++ Pocitac.java
@@ -0,0 +1,33 @@
+
+
+/*******************************************************************************
+ * Třída Pocitac představuje jeden počítač. Je součástí projektu na evidenci
+ * počítačů.
+ *
+ * @author    Luboš Pavlíček
+ * @version   0.00.000,  0.0.2006
+ */
+public class Pocitac
+{
+//== Datové atributy (statické i instancí)======================================
+    private long invCislo;
+    private String mistnost;
+    
+//##############################################################################
+//== Konstruktory a tovární metody =============================================
+
+    /***************************************************************************
+     *  konstruktor
+     *  @param invCislo inventarní číslo počítače
+     *  @param mistnost místnost, ve které je umístěn počítač
+     */
+    public Pocitac(long invCislo, String mistnost ){
+        this.invCislo = invCislo;
+        this.mistnost = mistnost;
+    }
+
+
+//== Nesoukromé metody (instancí i třídy) ===============================================
+//== Soukromé metody (instancí i třídy) ===========================================
+
+}
